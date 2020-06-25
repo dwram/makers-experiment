05097c44fbe071deae105fca8cd07a28ec63d6b2
dwram <danielim@hotmail.co.uk>
2020-06-25 14:18:53 +0100

    Input correct trial number and fixed text message send method

--- lib/twilio-test.rb
+++ lib/twilio-test.rb
@@ -1,5 +1,16 @@
 require 'bundler/setup'
 require 'twilio-ruby'
 
+# credentials to access API
 account_ssid = 'AC5f14dab093fe403c144c070dcc5dbb82'
-auth_token = ''
+auth_token = 'e3543f59eb34fdf907f422fe9097ef65'
+
+# client setup
+client = Twilio::REST::Client.new account_ssid, auth_token
+
+# send using client
+client.messages.create(
+    :from => '+12056971553',
+    :to => '+447736549438',
+    :body => 'Hey there!'
+)
